c09c039e66478a0239123c12884492ccd18c89dc
diynevala <riku@nevala.info>
2024-01-27 15:20:20 +0200

    Added enemy spawner

--- Src/main.gd
+++ Src/main.gd
@@ -1,12 +1,22 @@
 extends Node2D
 
+var enemyTemplate : PackedScene = load("res://Scenes/Components/enemy.tscn")
+
 var aimCursor = load("res://Assets/aim.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_custom_mouse_cursor(aimCursor)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_spawn():
+	var enemy = enemyTemplate.instantiate()
+	enemy.position.x = 100
+	enemy.position.y = 100
+	enemy.target = $Player
+	self.add_child(enemy)
+
