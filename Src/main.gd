50f230a57838a742a8f6663faf1bb12561f39dc2
Jussi Rasku <jussi.rasku@gmail.com>
2024-01-27 14:55:19 +0200

    Adds aim cursor, animates player and enemy-

--- Src/main.gd
+++ Src/main.gd
@@ -5,8 +5,7 @@ var aimCursor = load("res://Assets/aim.png")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_custom_mouse_cursor(aimCursor)
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
