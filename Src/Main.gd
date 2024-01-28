fe4f509379d8b3dd2d7f8290b84c320fd53731eb
Jussi Rasku <jussi.rasku@gmail.com>
2024-01-28 19:07:08 +0200

    Both bosses spawn

--- Src/Main.gd
+++ Src/Main.gd
@@ -3,11 +3,17 @@ extends Node2D
 var enemyTemplate : PackedScene = load("res://Scenes/Components/enemy.tscn")
 var aimCursor = load("res://Assets/aim.png")
 var rng = RandomNumberGenerator.new()
+var bosses = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_custom_mouse_cursor(aimCursor)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+
+	for child in get_children():
+		# Prefer to use type, but did not work
+		if "Boss" in child.name:
+			bosses.append(child)
 
 func _unhandled_input(event):
 	if event is InputEventKey:
@@ -19,9 +25,10 @@ func _process(_delta):
 	pass
 
 func _on_spawn():
+	var boss = bosses[randi() % bosses.size()]
 	var enemy = enemyTemplate.instantiate()
-	enemy.position.x = $Boss.position.x + rng.randi_range(-50,50)
-	enemy.position.y = $Boss.position.y + 50
+	enemy.position.x = boss.position.x + rng.randi_range(-50,50)
+	enemy.position.y = boss.position.y + 50
 	enemy.target = $Player
 	self.add_child(enemy)
 	
