65efe56280f24dbcfb80f59b515eb44fc9630b4c
Jussi Rasku <jussi.rasku@gmail.com>
2024-01-28 14:01:26 +0200

    Fix

--- Src/LookPoint.gd
+++ Src/LookPoint.gd
@@ -7,6 +7,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Divided by 4.0, why? Who knows! :D
-	var screenSize : Vector2 = -(DisplayServer.screen_get_size()/3.0)
+	var screenSize : Vector2 = -(DisplayServer.screen_get_size()/4.0)
 	self.position = screenSize+(($"../Player".position*3+get_global_mouse_position())/4.0)
 	#print(self.position)
